8fe0a558a5eb6ed26f7a6f6de4b9c8cb936805c9
elegance <1534672893@qq.com>
2022-03-26 22:25:08 +0800

    update

---
+++ banker/main/three.go
@@ -0,0 +1,268 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type Data struct {
+	Available  []int64   // 可利用资源向量
+	Allocation [][]int64 // 分配矩阵
+	Need       [][]int64 // 需求矩阵
+	Max        [][]int64 // 最大需求矩阵
+}
+
+// 初始化数据
+func InitData() Data {
+	available, allocation, need := GetData()
+	max := make([][]int64, len(allocation))
+	for i := range allocation {
+		max[i] = make([]int64, len(allocation[i]))
+	}
+	for i := range allocation {
+		for j := range allocation[i] {
+			max[i][j] = allocation[i][j] + need[i][j]
+		}
+	}
+	data := Data{
+		Available:  available,
+		Allocation: allocation,
+		Need:       need,
+		Max:        max,
+	}
+	return data
+}
+
+// 读取测试数据
+func GetData() ([]int64, [][]int64, [][]int64) {
+	var t []string
+	var content []byte
+	var available []int64
+	var allocation, need [][]int64
+	var err error
+	var file *os.File
+
+	file, err = os.Open("./available.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	content, _ = ioutil.ReadAll(file)
+	for _, item_j := range strings.Split(string(content), ",") {
+		f, err := strconv.ParseInt(item_j, 10, 64)
+		if err != nil {
+			panic(err)
+		}
+		available = append(available, f)
+	}
+
+	file, err = os.Open("./allocation.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	content, _ = ioutil.ReadAll(file)
+	t = strings.Split(string(content), "\r\n")
+	allocation = make([][]int64, len(t))
+	for i, item_i := range t {
+		for _, item_j := range strings.Split(item_i, ",") {
+			f, err := strconv.ParseInt(item_j, 10, 64)
+			if err != nil {
+				panic(err)
+			}
+			allocation[i] = append(allocation[i], f)
+		}
+	}
+
+	file, err = os.Open("./need.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	content, _ = ioutil.ReadAll(file)
+	t = strings.Split(string(content), "\r\n")
+	need = make([][]int64, len(t))
+	for i, item_i := range t {
+		for _, item_j := range strings.Split(item_i, ",") {
+			f, err := strconv.ParseInt(item_j, 10, 64)
+			if err != nil {
+				panic(err)
+			}
+			need[i] = append(need[i], f)
+		}
+	}
+
+	return available, allocation, need
+}
+
+// 安全性判断函数，可以判断当前时刻是否安全，以及判断输入请求向量后是否安全
+func IsSafe(data Data, p int64, request []int64, useRequest bool) ([]int64, string, bool) {
+	var safeSequence []int64
+	t := 0
+	finish := make([]bool, len(data.Allocation))
+	// 初始化finish数组，已经执行完成的进程置为true
+	for i := range finish {
+		finish[i] = true
+		for j := range data.Need[i] {
+			if data.Need[i][j] == 0 {
+				finish[i] = finish[i] && true
+			} else {
+				finish[i] = finish[i] && false
+			}
+		}
+	}
+	// 是否使用请求向量
+	if useRequest {
+		if data.ApplyRequest(p, request) {
+			safeSequence = append(safeSequence, p)
+			finish[p] = true
+		}
+	} else {
+		p = 0
+	}
+	for i := p; true && t != len(data.Allocation); i++ {
+		if IsAllTrue(finish) { // 当所有进程执行完break
+			break
+		}
+		data.Collect(i)     // 当可以分配资源时，分配资源
+		if data.IsDone(i) { // 当资源使用完毕，执行以下步骤
+			for k := range data.Allocation[i] {
+				data.Available[k] += data.Allocation[i][k]
+				data.Allocation[i][k] = 0
+				data.Need[i][k] = 0
+			}
+			safeSequence = append(safeSequence, i)
+			finish[i] = true
+			// 每当回收一次资源，将i和t置为最初始状态
+			i = -1
+			t = -1
+		}
+		if int(i) == len(data.Allocation)-1 {
+			i = 0
+		}
+		t += 1
+	}
+	// 当遍历一遍数组，仍无法分配资源，则表明已经造成死锁
+	if t == len(data.Allocation) {
+		return safeSequence, "不安全, 会导致死锁", false
+	}
+	return safeSequence, "安全", true
+}
+
+// 判断数组是否全为true
+func IsAllTrue(d []bool) bool {
+	for _, i := range d {
+		if !i {
+			return false
+		}
+	}
+	return true
+}
+
+// 判断进程是否得到足够资源
+func (data *Data) IsDone(p int64) bool {
+	for j := range data.Allocation[p] {
+		if data.Allocation[p][j] == data.Max[p][j] {
+			continue
+		} else {
+			return false
+		}
+	}
+	return true
+}
+
+// 足够分配资源时，分配资源
+func (data *Data) Collect(p int64) {
+	flag := true
+	for j := range data.Available {
+		if data.Need[p][j] <= data.Available[j] {
+			continue
+		} else {
+			flag = false
+			break
+		}
+	}
+	if flag {
+		for j := range data.Available {
+			data.Allocation[p][j] += data.Need[p][j]
+			data.Available[j] -= data.Need[p][j]
+			data.Need[p][j] = 0
+		}
+	}
+}
+
+// 往数据中加入请求变量
+func (data *Data) ApplyRequest(p int64, request []int64) bool {
+	for i := range request {
+		data.Available[i] -= request[i]
+		data.Allocation[p][i] += request[i]
+		data.Need[p][i] -= request[i]
+	}
+	if data.IsDone(p) {
+		for k := range data.Allocation[p] {
+			data.Available[k] += data.Allocation[p][k]
+			data.Allocation[p][k] = 0
+			data.Need[p][k] = 0
+		}
+		return true
+	}
+	return false
+}
+
+// 对数据进行深拷贝
+func deepCopy(src Data) Data {
+	var dst = new(Data)
+	b, _ := json.Marshal(src)
+	json.Unmarshal(b, &dst)
+	return Data{
+		Available:  dst.Available,
+		Allocation: dst.Allocation,
+		Need:       dst.Need,
+		Max:        dst.Max,
+	}
+}
+
+// 将字符数组转为int数组
+func stoi(request string) []int64 {
+	r := []int64{}
+	for _, item := range strings.Split(request, ",") {
+		f, _ := strconv.ParseInt(item, 10, 64)
+		r = append(r, f)
+	}
+	return r
+}
+
+// 打印table
+func (data *Data) PrintTable() {
+	fmt.Println("-------------------------------------------------------------")
+	fmt.Printf("Allocation: ")
+	fmt.Println(data.Allocation)
+	fmt.Printf("Need:       ")
+	fmt.Println(data.Need)
+	fmt.Printf("Available:   ")
+	fmt.Println(data.Available)
+	fmt.Printf("共有%d个进程，%d类资源\n输入进程号(%d-%d)，请求向量%d维(1,1,...)：", len(data.Allocation), len(data.Available), 0, len(data.Allocation), len(data.Available))
+}
+
+// 执行主函数
+func main() {
+	data := InitData()
+	var num int64
+	var request string
+	for {
+		data.PrintTable()
+		fmt.Scanf("%d %s\n", &num, &request)
+		safeSequence, content, flag := IsSafe(deepCopy(data), num, stoi(request), true)
+		fmt.Println(content)
+		if flag {
+			data.ApplyRequest(num, stoi(request))
+			fmt.Printf("其中一种安全序列是 ")
+			fmt.Println(safeSequence)
+		}
+		fmt.Printf("\n\n")
+	}
+}
---
+++ round_robin/main/two.go
@@ -0,0 +1,227 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+type Items struct {
+	Data []Item
+}
+
+// 定义每个进程执行过程所产生数据
+type Item struct {
+	P                     Process
+	FinishTime            float64 // 完成时间
+	TurnoverTime          float64 // 周转时间
+	TurnoverWithRightTime float64 // 带权周转时间
+}
+
+// 初始化一个空Item
+func InitItem(p Process) Item {
+	t := Item{
+		P:                     p,
+		FinishTime:            0,
+		TurnoverTime:          0,
+		TurnoverWithRightTime: 0,
+	}
+	return t
+}
+
+type Processs struct {
+	Data []Process
+}
+
+// 定义进程
+type Process struct {
+	Name       string  // 进程名
+	ArriveTime float64 // 到达时间
+	ServeTime  float64 // 服务时间
+	CountTime  float64 // 剩下服务时间
+}
+
+// 实现结构体排序接口
+type ByArriveTime []Process
+
+func (a ByArriveTime) Len() int           { return len(a) }
+func (a ByArriveTime) Less(i, j int) bool { return a[i].ArriveTime < a[j].ArriveTime }
+func (a ByArriveTime) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+
+// 实现结构体排序接口
+type ByServeTime []Process
+
+func (a ByServeTime) Len() int           { return len(a) }
+func (a ByServeTime) Less(i, j int) bool { return a[i].ServeTime < a[j].ServeTime }
+func (a ByServeTime) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+
+// 实现结构体排序接口
+type ByProcessName []Item
+
+func (a ByProcessName) Len() int           { return len(a) }
+func (a ByProcessName) Less(i, j int) bool { return a[i].P.Name < a[j].P.Name }
+func (a ByProcessName) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+
+// 添加队列尾元素
+func (p *Processs) Push(d Process) {
+	p.Data = append(p.Data, d)
+}
+
+// 取队列首元素
+func (p *Processs) Pop() Process {
+	t := p.Data[0]
+	p.Data = p.Data[1:]
+	return t
+}
+
+// 判断队列是否为空
+func (p *Processs) IsEmpty() bool {
+	return len(p.Data) == 0
+}
+
+// 读取测试数据
+func GetData() Processs {
+	file, err := os.Open("./data.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	content, _ := ioutil.ReadAll(file)
+	t := strings.Split(string(content), "\r\n")
+	names := strings.Split(t[0], ",")
+	arriveTimes := []float64{}
+	for _, i := range strings.Split(t[1], ",") {
+		f, err := strconv.ParseFloat(i, 32)
+		if err != nil {
+			panic(err)
+		}
+		arriveTimes = append(arriveTimes, f)
+	}
+	serveTimes := []float64{}
+	for _, i := range strings.Split(t[2], ",") {
+		f, err := strconv.ParseFloat(i, 32)
+		if err != nil {
+			panic(err)
+		}
+		serveTimes = append(serveTimes, f)
+	}
+	data := Processs{}
+	data.InsertData(names, arriveTimes, serveTimes)
+	return data
+}
+
+// 读取测试数据
+func (p *Processs) InsertData(names []string, arriveTimes, serveTimes []float64) {
+	for index := range names {
+		process := Process{
+			Name:       names[index],
+			ArriveTime: arriveTimes[index],
+			ServeTime:  serveTimes[index],
+			CountTime:  serveTimes[index],
+		}
+		p.Data = append(p.Data, process)
+	}
+}
+
+// 添加Item
+func (items *Items) AddItem(time float64, process Process) {
+	items.Data = append(items.Data, InitItem(process))
+	l := len(items.Data) - 1
+	items.Data[l].FinishTime = time
+	items.Data[l].TurnoverTime = time - items.Data[l].P.ArriveTime
+	items.Data[l].TurnoverWithRightTime = items.Data[l].TurnoverTime / items.Data[l].P.ServeTime
+}
+
+// 模拟进程RR调度
+func RR(data Processs, q float64) Items {
+	sort.Sort(ByArriveTime(data.Data)) // 保证数据以到达时间升序排列
+	processs := Processs{}
+	items := Items{}
+	time := 0.0
+	cnt := 0.0
+
+	for !data.IsEmpty() || !processs.IsEmpty() {
+		if !data.IsEmpty() {
+			if data.Data[0].ArriveTime == time { // 按时刻将进程加入待执行队列
+				front := data.Pop()
+				processs.Push(front)
+				fmt.Printf("到达进程为：%s\n", front.Name)
+			}
+		}
+		time += 1
+		if cnt == q && !processs.IsEmpty() { // 判断是否到达时间片及待执行的进程队列是否为空
+			processs.Push(processs.Pop())
+			cnt = 0
+		} else if cnt == q {
+			cnt = 0
+		}
+		fmt.Printf("当前时刻为：%.f-%.f, 当前运行进程为：%s\n", time-1, time, processs.Data[0].Name)
+		cnt += 1
+		if !processs.IsEmpty() {
+			if processs.Data[0].CountTime > 0 {
+				processs.Data[0].CountTime -= 1
+				if processs.Data[0].CountTime == 0 {
+					items.AddItem(time, processs.Pop()) // 服务完后，往结果队列添加进程
+					cnt = 0
+				}
+			}
+		}
+	}
+	sort.Sort(ByProcessName(items.Data))
+
+	return items
+}
+
+// 获取平均时间
+func GetAverageTime(items Items) (float64, float64) {
+	time1, time2 := 0.0, 0.0
+	for _, item := range items.Data {
+		time1 += item.TurnoverTime
+		time2 += item.TurnoverWithRightTime
+	}
+	l := float64(len(items.Data))
+	return time1 / l, time2 / l
+}
+
+// add Pause to windows exe
+func Pause() error {
+	buf := make([]byte, 1)
+	fmt.Println("\n输入 c ，然后回车以退出。")
+	for {
+		c, e := os.Stdin.Read(buf)
+		if c != 1 {
+			return e
+		}
+		if buf[0] == 'c' {
+			break
+		}
+	}
+	return nil
+}
+
+// Print break-line
+func PrintLine() {
+	fmt.Println("------------------------------------------------------")
+}
+
+func main() {
+	q := 1.0
+	fmt.Printf("请输入q：")
+	fmt.Scanf("%f", &q)
+	fmt.Printf("RR q=%.f\n", q)
+	PrintLine()
+	items := RR(GetData(), q)
+	PrintLine()
+	fmt.Println("进程名 到达时间 服务时间 完成时间 周转时间 带权周转时间")
+	for _, item := range items.Data {
+		fmt.Printf("%s     %.2f     %.2f     %.2f     %.2f     %.2f\n", item.P.Name,
+			item.P.ArriveTime, item.P.ServeTime, item.FinishTime, item.TurnoverTime,
+			item.TurnoverWithRightTime)
+	}
+	time1, time2 := GetAverageTime(items)
+	fmt.Printf("平均周转时间：%.2f 平均带权周转时间：%.2f\n", time1, time2)
+	Pause()
+}
